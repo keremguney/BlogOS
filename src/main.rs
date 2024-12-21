831918d0bf538a3db9105653a5de6357b1130a59
Kerem Güney <kerem@localhost.localdomain>
2024-12-21 19:44:50 +0300

    Keyboard-pc and hardware interrupts added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-
+use blog_os::println;
 use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello again{}", "!");
-    panic!("hi there");
-    loop {}
+    println!("Hello World{}", "!");
+
+    blog_os::init();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    blog_os::hlt_loop();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    blog_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
