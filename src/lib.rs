5c07e35352e2ca5f60ceb1487884861c906c6abb
Kerem Güney <kerem@localhost.localdomain>
2024-12-21 21:34:58 +0300

    mapping and paging implemented

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,14 @@
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
 pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod memory;
 
 pub fn init() {
     gdt::init();
@@ -70,10 +74,12 @@ pub fn hlt_loop() -> ! {
     }
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,34 @@
 
 use blog_os::println;
 use core::panic::PanicInfo;
+use bootloader::{entry_point, BootInfo};
+
+entry_point!(kernel_main);
+
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use x86_64::{VirtAddr, structures::paging::Page};
+    use blog_os::memory::BootInfoFrameAllocator;
+    use blog_os::memory;
+
     println!("Hello World{}", "!");
 
     blog_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("level 4 page table at: {:?}", level_4_page_table.start_address());
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
 
     #[cfg(test)]
     test_main();
