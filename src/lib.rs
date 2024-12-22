8cd365aa3ecef709d8d3dbc140bdc3939c21b0eb
Kerem Güney <kerem@localhost.localdomain>
2024-12-22 13:40:02 +0300

    heap allocation added

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,14 @@ use core::panic::PanicInfo;
 #[cfg(test)]
 use bootloader::{entry_point, BootInfo};
 
+extern crate alloc;
+
 pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 pub mod memory;
+pub mod allocator;
 
 pub fn init() {
     gdt::init();
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,19 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use blog_os::println;
 use core::panic::PanicInfo;
 use bootloader::{entry_point, BootInfo};
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 entry_point!(kernel_main);
 
-
 #[no_mangle]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::{VirtAddr, structures::paging::Page};
+    use x86_64::VirtAddr;
+    use blog_os::allocator;
     use blog_os::memory::BootInfoFrameAllocator;
     use blog_os::memory;
 
@@ -27,13 +30,22 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
-    
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+   
+    let heap_value = Box::new(41);
+    println!("heap value at {:p}", heap_value);
 
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     #[cfg(test)]
     test_main();
